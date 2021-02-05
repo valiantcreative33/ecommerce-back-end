0dc9259ca51381317dfc8d18e90d8869fa7f0edf
Ruben Matamoros <ruben@valiantcreative.net>
2021-02-05 16:52:15 -0500

    created Classes

--- models/index.js
+++ models/index.js
@@ -4,6 +4,16 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+Category.hasMany(Product);
+
+Product.belongsTo(Category);
+
+Product.belongsToMany(Tag, {
+  through: ProductTag
+});
+
+Tag.belongsToMany(Product);
+
 // Products belongsTo Category
 
 // Categories have many Products
